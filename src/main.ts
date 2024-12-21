fac9bfe53aa46781aa4a1d87a7092190e7fc265f
gregemax <gregemax700@example.com>
2024-12-21 21:10:59 +0100

    upload

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,9 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }),)
-  
-await app.listen(3000);
-console.log(`Application is running on port ${3000}`);
-
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
  // await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
