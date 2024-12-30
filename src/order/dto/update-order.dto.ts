94cebebbf1b587b7f96463528c71cb1c32ef486b
gregemax <gregemax700@example.com>
2024-12-30 17:13:03 +0100

    check

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDTO } from './create-order.dto';
+import { IsString } from 'class-validator';
+import { Optional } from '@nestjs/common';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDTO) {}
+export class UpdateOrderDto extends PartialType(CreateOrderDTO) {
+    @Optional()
+    status:string
+}
